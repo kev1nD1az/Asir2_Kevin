c94a26cdf3f2bd75c23fe4a41444c511f42affa7
unknown <kev1nD1az>
2019-10-04 21:25:32 +0200

    creacion variables

---
+++ variables.php
@@ -0,0 +1,10 @@
+<?php
+$a=50;
+$b=35;
+
+echo "primer numero ".$a.'<br/>';
+echo " segundo numero ".$b.'<br/>';
+echo " resta ".( $a - $b ).'<br/>';
+echo " suma ".( $a + $b ).'<br/>';
+echo " division ".( $a / $b ).'<br/>';
+?>
