152156865d718c21a4fc620c49effec42d6a94d1
unknown <kev1nD1az>
2019-10-07 17:20:58 +0200

    2b y 2c

---
+++ 2b.php
@@ -0,0 +1,27 @@
+<?php
+
+function calcula(){
+$a=2;
+$b=8;
+echo '<div>';
+echo 'Suma de '.$a.' + '.$b.' = ';
+echo $a+$b;
+echo '</div>';
+
+echo '<div>';
+echo 'Resta de '.$a.' - '.$b.' = ';
+echo $a-$b;
+echo '</div>';
+
+echo '<div>';
+echo 'Multiplicación de '.$a.' x '.$b.' = ';
+echo $a*$b;
+echo '</div>';
+
+echo '<div>';
+echo 'División de '.$a.' / '.$b.' = ';
+echo $a/$b;
+echo '</div>'; 
+}
+calcula();
+?>	
---
+++ 2c.php
@@ -0,0 +1,26 @@
+<?php
+
+function calcula($a,$b){
+
+echo '<div>';
+echo 'Suma de '.$a.' + '.$b.' = ';
+echo $a+$b;
+echo '</div>';
+
+echo '<div>';
+echo 'Resta de '.$a.' - '.$b.' = ';
+echo $a-$b;
+echo '</div>';
+
+echo '<div>';
+echo 'Multiplicación de '.$a.' x '.$b.' = ';
+echo $a*$b;
+echo '</div>';
+
+echo '<div>';
+echo 'División de '.$a.' / '.$b.' = ';
+echo $a/$b;
+echo '</div>'; 
+}
+calcula(5,5);
+?>	
