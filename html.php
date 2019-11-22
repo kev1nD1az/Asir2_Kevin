3ce9c8b85cfcdb33727234bf91abb3bbd7982804
unknown <kev1nD1az>
2019-11-22 19:36:06 +0100

    version 9

--- html.php
+++ html.php
@@ -1,34 +1,30 @@
 <html>
 	<head>
-		<title> Pagina HTML</title>
-		<style>
-			table, th, td {
-			border: 1px solid black;
-			}
-		</style>
+	<meta charset="UTF-8"/>
+		<title> RIOS</title>
 	</head>
 	<body bgcolor="#E6E6FA">
-		<h1>Hola Mundo</h1>
-			<p>Esta pagina es una demostracion del uso de html.
-		<h2>¿De que parte del Mundo?</h2>
+		<h1>Bienvenidos a españa</h1>
+			<p>En esta pagina vamos a ver las provincia de autonomías bañadas por el Cantábrico.
+		<h2>Las principales son:</h2>
 		 <center><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png" width="500" height="300"></center>
-		 <p> ¿Te gusta España? Mete tus datos. </br><font size="3" color="red"> <i><u>VIVA ESPAÑA</u></i></font>
+		 <p> ¿Te gusta España? Mete tus datos. </br><font size="3" color="red"> <b><u>VIVA ESPAÑA</u></b></font>
 			 <form>
 				<input type="email" name="email"/>
 				<button>Enviar</button>
 			</form>
-		<h3>Encargados de la pagina</h3>
+		<h3>Tabla</h3>
 			
-	<table style="width:100%">
-	  <tr>
-		<th>Autonomia</th>
-		<th>Provincia</th>
+	<table border="1" width="1000" align="center">
+	  <tr align="center">
+		<th bgcolor="#ffff00">Autonomia</th>
+		<th bgcolor="#ff0000">Provincia</th>
 	  </tr>
-	  <tr>
+	  <tr align="center">
 		<td>Cantabria</td>
 		<td>Santander</td>
 	  </tr>
-	  <tr>
+	  <tr align="center">
 		<td>Asturias</td>
 		<td>Castropol</td>
 	  </tr>
