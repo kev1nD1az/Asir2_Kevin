1533662c965fb2d7089c560d8c196081c9c1fce6
unknown <kev1nD1az>
2020-01-13 15:41:07 +0100

    conn.php

---
+++ html5up-ethereal/conn.php
@@ -0,0 +1,3 @@
+<?php
+$conectar = new mysqli('localhost','id11961169_kevin','12345678','id11961169_formularios');
+?>
--- html5up-ethereal/index.php
+++ html5up-ethereal/index.php
@@ -1,32 +1,28 @@
 <?php
 	//conectamos Con el servidor
-	$conectar = new mysqli('localhost','id11961169_kevin','12345678','id11961169_formularios');
-	//verificamos la conexion
-	if(!$conectar){
-		echo"No Se Pudo Conectar Con El Servidor";
-	}else{
-
+	include 'conn.php'; //$conectar = new mysqli(...);
+	if(isset($_POST['name'])){
+		//recuperar las variables
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$message=$_POST['message'];
+		//hacemos la sentencia de sql
+		$sql="INSERT INTO Datos VALUES('$name',
+									   '$email',
+									   '$message')";
+		//ejecutamos la sentencia de sql
+		$ejecutar=$conectar->query($sql);
+		//verificamos la ejecucion
+		if(!$ejecutar){
+			echo"Hubo Algun Error";
+		}else{
+			echo"Datos Guardados Correctamente<br><a href='index.html'>Volver</a>";
+		}
 	}
-	//recuperar las variables
-	$name=$_POST['name'];
-	$email=$_POST['email'];
-	$message=$_POST['message'];
-	//hacemos la sentencia de sql
-	$sql="INSERT INTO Datos VALUES('$name',
-								   '$email',
-								   '$message')";
-	//ejecutamos la sentencia de sql
-	$ejecutar=$conectar->query($sql);
-	//verificamos la ejecucion
-	if(!$ejecutar){
-		echo"Hubo Algun Error";
-	}else{
-		echo"Datos Guardados Correctamente<br><a href='index.html'>Volver</a>";
-	}
-	$numero_visitantes=$conectar->query(
-	"SELECT COUNT (*) FROM Datos"
-	);
-	fetch_row()[0]
+	
+		$numero_visitantes=$conectar->query(
+		"SELECT COUNT(*) FROM Datos"
+		)->fetch_row()[0];
 	
 ?>
 <!DOCTYPE HTML>
